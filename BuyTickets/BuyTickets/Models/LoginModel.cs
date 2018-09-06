23d65a26f9fca428abd265c5ec6e095d6ac5669d
DESKTOP-J4VB9R6\Aldo <acaaguirre@gmail.com>
2018-09-06 12:24:02 -0500

    Test project for buying tickets. V1.0 Login page Not functional Errors in UI but resposiveness is working.

---
+++ BuyTickets/BuyTickets/Models/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuyTickets.Models
+{
+    public class LoginModel
+    {        
+        [DataType(DataType.EmailAddress)]  
+        [Required(ErrorMessage ="Nickname is required")]
+        [Display(Name ="Nickname")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Secret word required")]
+        [Display(Name = "Your secret word")]
+        public string Password { get; set; }
+
+    }
+}
